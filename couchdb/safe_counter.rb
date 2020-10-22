7a6c218c5b1ecf6d4b5c5ff042eb892b12bc881b
James Coglan <jcoglan@gmail.com>
2020-10-22 16:03:23 +0100

    CouchDB counter examples
    These two examples demonstrate how CouchDB prevents lost updates using
    compare-and-swap; the `unsafe_counter` example fails with an exception
    while `safe_counter` handles `Conflict` errors and runs successfully.

---
+++ couchdb/safe_counter.rb
@@ -0,0 +1,25 @@
+require "bundler/setup"
+require "couchrest"
+
+DB = "http://admin:admin@localhost:5984/examples"
+CouchRest.put(DB)
+CouchRest.put("#{DB}/counter", "value" => 0)
+
+def inc_counter
+  counter = CouchRest.get("#{DB}/counter")
+  counter["value"] += 1
+  CouchRest.put("#{DB}/counter", counter)
+rescue CouchRest::Conflict
+  retry
+end
+
+threads = (1..4).map do
+  Thread.new do
+    100.times { inc_counter }
+  end
+end
+
+threads.each(&:join)
+p CouchRest.get("#{DB}/counter")
+
+CouchRest.delete(DB)
